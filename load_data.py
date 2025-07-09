7e993675a736926475df1e5b32de577f489f39da
Gutu Rarie <guturarie@gmail.com>
2025-07-09 14:32:38 +0300

    Temporarily disable create_user_profile signal during loaddata

--- load_data.py
+++ load_data.py
@@ -1,10 +1,13 @@
-import django
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cpms_project.settings")
-django.setup()
-
 from django.core.management import call_command
+from django.db.models.signals import post_save
+from cpms_app.models import User
+from cpms_app.signals import create_user_profile
 
+# Disconnect the signal temporarily
+post_save.disconnect(create_user_profile, sender=User)
+
+# Load the fixture
 call_command('loaddata', 'data.json')
-print("✅ Data loaded successfully.")
+
+# Reconnect the signal
+post_save.connect(create_user_profile, sender=User)
